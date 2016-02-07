8a4fecfe7545c176e4be0d2c145a4329672192d2
Tom Barwick <tb1414@ic.ac.uk>
2016-02-07 02:21:06 +0000

    Allows robots to belong to a grid

--- Robot.rb
+++ Robot.rb
@@ -2,10 +2,11 @@ class Robot
 
 	attr_reader :x, :y, :orent
 
-	def initialize(x, y, orient)
+	def initialize(x, y, orient, grid)
 		@x = x
 		@y = y
 		@orient = orient
+		@grid = grid
 		@lost = false
 	end
 
